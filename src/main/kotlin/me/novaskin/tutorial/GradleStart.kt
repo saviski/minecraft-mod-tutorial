33181a65b7d0abff14356f9cf3dccb08083d96a5
Marcelo Saviski <marcelosaviski@gmail.com>
2018-06-03 03:16:42 -0300

    forge and kotlin initial setup

---
+++ src/main/kotlin/me/novaskin/tutorial/GradleStart.kt
@@ -0,0 +1,6 @@
+package me.novaskin.tutorial
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(modid = "tutorial", name = "tutorial", version = "0.0", modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
+object GradleStart 
